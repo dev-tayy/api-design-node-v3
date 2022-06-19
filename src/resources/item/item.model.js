da728ffc27ae353a0d5447bc21f50bc37d04f9e9
iloveteajay <atytjani@gmail.com>
2022-06-19 02:20:32 +0100

    Lesson 2 done :rocket:

--- src/resources/item/item.model.js
+++ src/resources/item/item.model.js
@@ -1,4 +1,15 @@
 import mongoose from 'mongoose'
 
-const itemSchema = new mongoose.Schema({}, { timestamps: true })
+const itemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 50
+    },
+    description: String
+  },
+  { timestamps: true }
+)
 export const Item = mongoose.model('item', itemSchema)
--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,20 @@
 import { Router } from 'express'
+// import controllers from './item.controllers'
 
+const controller = (req, res) => {
+  res.send('Welcome to my world Motherfocka')
+}
 const router = Router()
+
+router
+  .route('/')
+  .get(controller)
+  .post(controller)
+
+router
+  .route('/:id')
+  .get(controller)
+  .put(controller)
+  .delete(controller)
 
 export default router
